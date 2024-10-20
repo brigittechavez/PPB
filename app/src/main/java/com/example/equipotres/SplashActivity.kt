ef742463a579e2026f7bc24612d6d29c0b0ad703
GironDev <jhon.alejandro.giron@correounivalle.edu.co>
2024-10-19 22:22:32 -0500

    Added counter and button functionality to MainActivity

--- app/src/main/java/com/example/equipotres/SplashActivity.kt
+++ app/src/main/java/com/example/equipotres/SplashActivity.kt
@@ -5,18 +5,21 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.equipotres.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
 
-        // Simular un retraso de 5 segundos
+        // Simular un retraso de 5 segundos antes de ir a MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
-            // Iniciar MainActivity
             startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            finish() // Termina la actividad de splash
         }, 5000) // 5000 milisegundos = 5 segundos
     }
 }
