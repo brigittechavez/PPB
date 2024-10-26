25d0e5778bdaf4f78de55e6ae3ebafc0a19c26db
GironDev <jhon.alejandro.giron@correounivalle.edu.co>
2024-10-26 01:25:50 -0500

    toolbar and icons, add sound and bottle new, add animation button

--- app/src/main/java/com/example/equipotres/MainActivity.kt
+++ app/src/main/java/com/example/equipotres/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.equipotres
 
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,13 +18,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var countdownTimer: CountDownTimer
+    private lateinit var mediaPlayer: MediaPlayer
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound_main)
+        mediaPlayer.isLooping = true // Hace que el sonido se repita en bucle
+        mediaPlayer.start() // Inicia el sonido de fondo
+
+        // Iniciar el contador regresivo
+        startCountdown()
+
         // Configurar el botón "Presióname" para que parpadee
-        makeButtonBlink(binding.pressMeButton)
+        makeButtonBlink(binding.bButton)
     }
 
     override fun onBackPressed() {
@@ -30,6 +42,21 @@ class MainActivity : AppCompatActivity() {
         finishAffinity() // Cierra todas las actividades y sale de la aplicación
     }
 
+    private fun startCountdown() {
+        binding.contador.text = "3"
+
+        countdownTimer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsLeft = (millisUntilFinished / 1000).toInt()
+                binding.contador.text = secondsLeft.toString()
+            }
+
+            override fun onFinish() {
+                binding.contador.text = "0"
+                binding.contador.visibility = View.GONE
+            }
+        }.start()
+    }
 
     private fun makeButtonBlink(button: Button) {
         val animator = ObjectAnimator.ofFloat(button, "alpha", 0f, 1f)
@@ -37,5 +64,19 @@ class MainActivity : AppCompatActivity() {
         animator.repeatMode = ValueAnimator.REVERSE
         animator.duration = 500
         animator.start()
+    }
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.pause() // Pausa el sonido si la actividad se pausa
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer.start() // Reanuda el sonido cuando la actividad vuelve a primer plano
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release() // Libera los recursos del MediaPlayer
     }
 }
